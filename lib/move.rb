6ab366b6169aae3c98ef7f1e7670a6d0ee83ef12
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-29 20:16:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,7 @@ end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def update_array_at_with(array, index, value)
-  array[index] = value 
+  array[index] = value
 end
 
 update_array_at_with(board, 0, "X")
