e092279cc0dd5ecd1731f259c07ee359901ea2b4
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-29 19:57:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-user_input = "user_input"
+user_input = "user_input" - 1
 
 def input_to_index(user_input)
   puts "#{user_input}"
