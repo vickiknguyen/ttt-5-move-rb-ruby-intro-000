a2ee27b61b63c7601c9049ac5505376989986667
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-29 18:44:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +6,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+
 
 # code your input_to_index and move method here!
