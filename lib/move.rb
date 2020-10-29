a1b97e4c3592136ddde57f2265a85c5ea353abc6
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-29 22:32:37 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -20,8 +20,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player="X")
-   board[index] = player
+def move(board, index, value="X")
+   board[index] = value
 end
 
 
