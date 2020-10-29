42872026533ec03adb70558667c1ac87c6efbb21
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-29 20:16:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,5 +11,11 @@ def input_to_index(user_input)
   "#{user_input}".to_i - 1
 end
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value 
+end
+
+update_array_at_with(board, 0, "X")
 
 # code your input_to_index and move method here!
